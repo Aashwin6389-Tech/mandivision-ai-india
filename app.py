2a5759a1949a4bf64a1db1a9388faa2849a9f260
Aashwin6389-Tech <tripathiaashwin935@gmail.com>
2026-09-07 19:48:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import streamlit as st
 import pandas as pd
 import numpy as np
 import hashlib
@@ -1818,4 +1818,3 @@ st.divider()
 st.caption(
     "🇮🇳 MandiVision AI | Smart Agriculture Decision Support System"
 )
-
